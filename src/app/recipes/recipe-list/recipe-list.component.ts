410a49f1660d1cbeecfd16517281cd95ae4ba664
Modlich Adam <adam.modlich13@gmail.com>
2023-05-14 20:02:27 +0200

    Adding-http-requests-to-firebase

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,6 +4,7 @@ import { Recipe } from '../recipe.model';
 import { RecipesService } from '../recipes.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -14,10 +15,10 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;
 
-  constructor(private recipesService: RecipesService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private dataStorageService: DataStorageService, private recipesService: RecipesService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.recipes = this.recipesService.getRecipes();
+    this.dataStorageService.fetchRecipes().subscribe()
     this.subscription = this.recipesService.recipesChanged.subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
     })
