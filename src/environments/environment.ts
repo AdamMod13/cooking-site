da2fe5ec6a6f93c247148fc9de1bfa6b30af0ce8
Modlich Adam <adam.modlich13@gmail.com>
2023-05-21 14:27:49 +0200

    Managing environment varaibles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyAZO_veAvnXkjGl6So1Rr58PNG3kLnJjP0'
 };
 
 /*
